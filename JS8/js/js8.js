312446bd46db9e1025f96c81dc5f4ad7172d2632
Suvi Tuomela <suvituom@ety-a2123b-26.edu.wks.metropolia.fi>
2018-11-21 09:25:58 +0200

    uusia juttuja

--- JS8/js/js8.js
+++ JS8/js/js8.js
@@ -1,18 +1,28 @@
-const hakunappi = document.querySelector('button');
-hakunappi.addEventListener('click', etsiSarjoja);
-
 function etsiSarjoja() {
-  const haku = document.getElementById('haku'.value);
-fetch('http://api.tvmaze.com/singlesearch/shows?q=${haku}')             // Käynnistetään haku. Vakiometodi on GET.
-    .then(function(vastaus){        // Sitten kun haku on valmis,
-      return vastaus.json();        // muutetaan ladattu tekstimuotoinen JSON JavaScript-olioksi
-    }).then(function(json){         // Sitten otetaan ladattu data vastaan ja
-  naytaKuva(json);
-});
+  const haku = document.getElementById('hakuteksti').value
+  fetch(`https://api.tvmaze.com/search/shows?q=${haku}`)
+  .then(vastaus => vastaus.json())
+  .then(series => {
+    console.log(series);
+    const app = document.getElementById('app');
+    app.innerHTML = series.map(({show}) => `
+<div class="container">
+            <div id="kuva">
+            ${show.image ? `<img src="${show.image.medium}">` : ''}
+            </div>
+            <div>
+               <h4>${show.name}</h4>
+                    <h7><a href="${show.url}">${show.url}</a></h7><br>
+                    <h7>${show.genres}</h7>
+                    <h7>${show.summary}</h7><br>
+               
+               </div>
+                 </div>
+                 
+`).join('');
+  })
 }
 
-  function naytaKuva() {
-    const osoite = "http://api.tvmaze.com/singlesearch/shows?q=$haku"; // 'Kuvat' taulukon toisen objektin 'osoite' ominaisuus
+const nappi = document.getElementById('hakunappi');
+nappi.addEventListener('click', etsiSarjoja);
 
-    document.querySelector('a').href = osoite;
-  }
